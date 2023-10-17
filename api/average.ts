a72c9c079558334f482da9f35e47ce08fe5f9256
Cédric <seedy.dupuis@gmail.com>
2023-10-17 18:25:11 +0200

    fix(api): throw error in french

--- api/average.ts
+++ api/average.ts
@@ -1,7 +1,7 @@
 
 export const getAverage = async (ids: number[]) => {
     if (ids.length === 0) {
-        throw new Error("No users selected");
+        throw new Error("Pas d'utilisateur sélectionné");
     }
     const res = await fetch(`https://infallible-tereshkova-717266.netlify.app/.netlify/functions/server/average?ids=[${ids.join(',')}]`, { method: 'GET' });
     const result = await (res.json() as Promise<Average>);
